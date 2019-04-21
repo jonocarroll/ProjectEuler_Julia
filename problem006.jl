dd0a8522251dda7e5dbcb4de94347d8a732cd62b
Jonathan Carroll <jono@jcarroll.com.au>
2019-04-21 14:42:23 +0930

    problem007 solved

--- problem006.jl
+++ problem006.jl
@@ -4,6 +4,8 @@
 ## (1 + 2 + ... + 10)² = 55² = 3025
 ## Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
 ## Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+
+## check original statement
 sumsq = sum([i^2 for i in 1:10])
 sqsum = sum(1:10).^2
 sqsum - sumsq
